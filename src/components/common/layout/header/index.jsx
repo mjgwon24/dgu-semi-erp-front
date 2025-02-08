b9ae0075d003eb927981236530808d8e025bda3d
seokjin925 <153346471+seokjin925@users.noreply.github.com>
2025-02-08 17:48:30 +0900

    feat: headerLayout 	•	헤더 레이아웃 구현 	•	상단에 고정된 헤더 스타일 적용 (bg-white, border-b, flex 레이아웃 활용) 	•	justify-end을 사용하여 오른쪽 정렬 	•	알림 아이콘 추가 	•	@mui/material의 IconButton 및 NotificationsIcon 사용 	•	스타일링 적용 (text-[#2C2C2C]) 	•	Redux 상태 기반 사용자 정보 표시 	•	useSelector를 사용하여 Redux에서 userName 가져오기 	•	로그인한 경우, 사용자 프로필 UI(원형 배경 + 텍스트) 표시 	•	로그인하지 않은 경우, /login 페이지로 이동하는 로그인 버튼 표시 	•	스타일링 및 유지보수성 개선 	•	Tailwind CSS 적용 (px-6 py-4, rounded-full 등) 	•	버튼 및 아이콘 hover 효과 추가

--- src/components/common/layout/header/index.jsx
+++ src/components/common/layout/header/index.jsx
@@ -8,23 +8,23 @@ export default function HeaderLayout(props) {
     const userName = useSelector((state) => state.user.userName);
 
     return (
-        <div className="flex items-center justify-end px-6 py-4 bg-white border-b-[1px] border-gray-300 w-full h-full space-x-4">
+        <div className="flex items-center justify-end px-6 py-4 bg-[#F7F7F7] border-b-[1px] border-gray-300 w-full h-full space-x-4">
             {/* 알림 아이콘 */}
             <div>
                 <IconButton>
-                    <NotificationsIcon className="text-[#2C2C2C]" />
+                    <NotificationsIcon fontSize="large" className="text-[#4C545B]" />
                 </IconButton>
             </div>
 
             {/* 사용자 이름 또는 로그인 버튼 */}
             <div>
                 {userName ? (
-                    <div className="w-14 h-14 bg-[#F4F4F4] flex items-center justify-center rounded-full text-[#303030] font-[Pretendard] text-[16px] font-semibold leading-normal">
+                    <div className="w-14 h-14 bg-[#212121] flex items-center justify-center rounded-full text-[white] font-[Pretendard] text-[16px] font-semibold leading-normal">
                         {userName}
                     </div>
                 ) : (
                     <Link href="/login">
-                        <button className="text-white bg-gray-600 px-4 py-2 rounded-md hover:bg-[#F4F4F4]">
+                        <button className="text-white bg-black px-4 py-2 rounded-md hover:bg-[#F4F4F4]">
                             로그인
                         </button>
                     </Link>
