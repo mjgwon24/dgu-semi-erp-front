c94bd4815fc615d7c4321cbf5fd4896bb815291e
mjgwon24 <alswlchlrh8@naver.com>
2025-02-17 18:40:16 +0900

    update: 사이드바, 헤더 UI 디테일 개선

--- src/components/common/layout/sidebar/index.jsx
+++ src/components/common/layout/sidebar/index.jsx
@@ -7,64 +7,75 @@ import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import PersonSearchIcon from "@mui/icons-material/PersonSearch";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
-
 export default function SideBarLayout() {
     const [isBudgetMenuOpen, setBudgetMenuOpen] = useState(false);
 
     return (
-        <div className="bg-[#212121] h-full flex flex-col p-6 text-white">
-            <Link href="/main">
-                <div className="text-white font-[Pretendard] pt-3 text-[16px] font-extrabold leading-normal cursor-pointer">
-                    동국대학교<br />동아리행정정보시스템
-                </div>
+        <div className="bg-[#212121] h-full flex flex-col p-6 min-w-[205px]">
+            <Link href="/main" className="cursor-pointer">
+                <h1 className="text-white weight-700 text-[17px]">동국대학교<br/>동아리행정정보시스템</h1>
             </Link>
 
-            <ul className="flex flex-col space-y-9 mt-8 pt-7">
-                <li className="flex items-center space-x-3 text-white text-[16px] font-medium hover:text-blue-600 cursor-pointer">
-                    <ArticleIcon fontSize="medium" />
-                    <span>공지사항</span>
+            <ul className="flex flex-col space-y-5 pt-10">
+                <li className="flex items-center text-white cursor-pointer weight-700">
+                    <Link href="/main" className="cursor-pointer flex items-center space-x-3">
+                        <ArticleIcon fontSize="small" />
+                        <span>공지사항</span>
+                    </Link>
                 </li>
-                <li className="flex items-center space-x-3 text-white text-[16px] font-medium hover:text-blue-600 cursor-pointer">
-                    <GroupsIcon fontSize="medium" />
-                    <span>동아리</span>
+                <li className="flex items-center text-white cursor-pointer weight-700">
+                    <Link href="/main" className="cursor-pointer flex items-center space-x-3">
+                        <GroupsIcon fontSize="small" />
+                        <span>동아리</span>
+                    </Link>
                 </li>
                 <li>
                     <div
                         onClick={() => setBudgetMenuOpen(!isBudgetMenuOpen)}
-                        className="flex items-center space-x-3 text-white text-[16px] font-medium hover:text-blue-600 cursor-pointer"
+                        className="flex items-center space-x-3 text-white cursor-pointer weight-700"
                     >
-                        <CreditCardIcon fontSize="medium" />
+                        <CreditCardIcon fontSize="small" />
                         <span>예산</span>
                     </div>
                     {isBudgetMenuOpen && (
-                        <ul className="ml-4 mt-2 flex flex-col space-y-3">
-                            <li className="text-gray-300 text-[14px] font-medium hover:text-blue-400 cursor-pointer">
-                                예산 계획
+                        <ul className="ml-8 mt-2 flex flex-col space-y-2">
+                            <li className="text-gray-300 cursor-pointer text-[15px] weight-700">
+                                <Link href="/budgetPlan" className="cursor-pointer flex items-center space-x-3">
+                                    예산 계획
+                                </Link>
                             </li>
-                            <li className="text-gray-300 text-[14px] font-medium hover:text-blue-400 cursor-pointer">
-                                예산 사용 내역
+                            <li className="text-gray-300 cursor-pointer text-[15px] weight-700">
+                                <Link href="/budgetUsage" className="cursor-pointer flex items-center space-x-3">
+                                    예산 사용 내역
+                                </Link>
                             </li>
-                            <li className="text-gray-300 text-[14px] font-medium hover:text-blue-400 cursor-pointer">
-                                예산 보고서
+                            <li className="text-gray-300 cursor-pointer text-[15px] weight-700">
+                                <Link href="/budgetReport" className="cursor-pointer flex items-center space-x-3">
+                                    예산 보고서
+                                </Link>
                             </li>
                         </ul>
                     )}
                 </li>
-                <li className="flex items-center space-x-3 text-white text-[16px] font-medium hover:text-blue-600 cursor-pointer">
-                    <PointOfSaleIcon fontSize="medium" />
-                    <span>통장관리</span>
+                <li className="flex items-center text-white weight-700 cursor-pointer">
+                    <Link href="/bankbookManagement" className="cursor-pointer flex items-center space-x-3">
+                        <PointOfSaleIcon fontSize="small" />
+                        <span>통장관리</span>
+                    </Link>
                 </li>
-                <li className="flex items-center space-x-3 text-white text-[16px] font-medium hover:text-blue-600 cursor-pointer">
-                    <PersonSearchIcon fontSize="medium" />
-                    <span>인원관리</span>
+                <li className="flex items-center text-white weight-700 cursor-pointer">
+                    <Link href="/peopleManagement" className="cursor-pointer flex items-center space-x-3">
+                        <PersonSearchIcon fontSize="small" />
+                        <span>인원관리</span>
+                    </Link>
                 </li>
-                <li className="flex items-center space-x-3 text-white text-[16px] font-medium hover:text-blue-600 cursor-pointer">
-                    <CalendarMonthIcon fontSize="medium" />
-                    <span>일정</span>
-
+                <li className="flex items-center text-white font-normal cursor-pointer">
+                    <Link href="/main" className="cursor-pointer flex items-center space-x-3">
+                        <CalendarMonthIcon fontSize="small" />
+                        <span>일정</span>
+                    </Link>
                 </li>
             </ul>
         </div>
     );
-
 }
