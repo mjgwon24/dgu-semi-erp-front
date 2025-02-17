3e0e88397eaa009dba3097cc2c1000d533296ef7
mjgwon24 <alswlchlrh8@naver.com>
2025-02-17 18:33:26 +0900

    update: 사이드바, 헤더 UI 디테일 개선

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import store from "../redux/store";  // ✅ store 경로 6-feature 기준 유지
+import store from "../redux/store";
 import SideBarLayout from "@/src/components/common/layout/sidebar";
 import HeaderLayout from "@/src/components/common/layout/header";
 
@@ -11,16 +11,16 @@ export default function App({ Component, pageProps }) {
     return (
         <Provider store={store}>
             <QueryClientProvider client={queryClient}>
-                <div className="flex h-screen">
-                    <div className="w-[12%] bg-gray-100">
-                        <SideBarLayout />  {/* ✅ Sidebar 유지 */}
+                <div className="flex min-h-screen">
+                    <div className="">
+                        <SideBarLayout />
                     </div>
-                    <div className="flex-grow flex flex-col border-l-[1px] border-gray-300">
-                        <div className="h-[10%]">
-                            <HeaderLayout />  {/* ✅ Header 유지 */}
+                    <div className="flex-grow flex flex-col">
+                        <div className="">
+                            <HeaderLayout />
                         </div>
                         <div className="flex-grow">
-                            <Component {...pageProps} />  {/* ✅ 6-feature 기반 유지 */}
+                            <Component {...pageProps} />
                         </div>
                     </div>
                 </div>
