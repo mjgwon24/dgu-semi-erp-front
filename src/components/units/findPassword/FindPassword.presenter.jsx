14bb2597a5fe0229391cdb63b780eab5d012241b
Hwajinpo <hwajindeck@gmail.com>
2025-03-15 20:37:05 +0900

    update: 가로 패딩 및 텍스트 간격 조정

--- src/components/units/findPassword/FindPassword.presenter.jsx
+++ src/components/units/findPassword/FindPassword.presenter.jsx
@@ -9,11 +9,11 @@ export default function FindPasswordUI({
 }) {
     return (
         <div className="flex w-full h-screen justify-center items-center" style={{color: "#F7F7F7"}}>
-            <form className="flex flex-col items-center border rounded-2xl shadow-xl bg-[#FFFFFF] h-4/5 py-48 px-24 gap-12 w-2/5" onSubmit={handleSubmit}>
+            <form className="flex flex-col items-center border rounded-2xl shadow-xl bg-[#FFFFFF] h-4/5 py-48 px-48 gap-12 w-2/5" onSubmit={handleSubmit}>
                 <div className="flex flex-col items-center gap-1">
                     <label className="text-4xl font-bold" style={{color: "#247CE6"}}>동국대학교</label>
                     <label className="text-4xl font-bold" style={{color: "#000000"}}>동아리 <span style={{color: "#247CE6"}}>행정</span> 정보 시스템</label>
-                    <label className="text-xl" style={{color: "#3C3C3C"}}>비밀번호를 잊으셨나요?</label>
+                    <label className="text-xl pt-2" style={{color: "#3C3C3C"}}>비밀번호를 잊으셨나요?</label>
                 </div>
 
                 <div className="flex flex-col gap-2 w-full text-xl">
