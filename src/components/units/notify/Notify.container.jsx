17b14171cedcc150459d18a64662d800fefb60ae
mjgwon24 <alswlchlrh8@naver.com>
2025-03-17 15:32:06 +0900

    update: 알림 페이지 메뉴 선택 변수 state 전환

--- src/components/units/notify/Notify.container.jsx
+++ src/components/units/notify/Notify.container.jsx
@@ -2,6 +2,67 @@ import NotifyUI from "@/src/components/units/notify/Notify.presenter";
 import {useState} from "react";
 
 export default function Notify() {
+    /**
+     * 알림 사이드 메뉴
+     * - 한 페이지당 7개의 알림
+     */
+    const [selectedMenu, setSelectedMenu] = useState("동아리");
+    const [data, setData] = useState([
+            {
+                title: "동아리 가입 승인",
+                description: "동아리 가입이 승인되었습니다.",
+                date: "2021-09-01",
+                isNew: true
+            },
+        {
+            title: "동아리 가입 승인",
+            description: "동아리 가입이 승인되었습니다.",
+            date: "2021-09-01",
+            isNew: true
+        },
+        {
+            title: "동아리 가입 승인",
+            description: "동아리 가입이 승인되었습니다.",
+            date: "2021-09-01",
+            isNew: false
+        },
+        {
+            title: "동아리 가입 승인",
+            description: "동아리 가입이 승인되었습니다.",
+            date: "2021-09-01",
+            isNew: false
+        },
+        {
+            title: "동아리 가입 승인",
+            description: "동아리 가입이 승인되었습니다.",
+            date: "2021-09-01",
+            isNew: false
+        },
+        {
+            title: "동아리 가입 승인",
+            description: "동아리 가입이 승인되었습니다.",
+            date: "2021-09-01",
+            isNew: false
+        },
+            {
+                title: "결제 승인 요청",
+                description: "[DEVELOPER] 2025.03.10 카누 외 1건 승인 요청",
+                date: "2025.03.15",
+                isNew: false
+            }
+        ]);
+
+    const sideMenus = {
+        "동아리": "/api/notify/club?page=",
+        "예산": "/api/notify/budget?page=",
+        "통장 관리": "/api/notify/account?page=",
+    }
+
+    // api 호출 -> setData 필요
+
+    /**
+     * 페이지네이션
+     */
     const [currentPage, setCurrentPage] = useState(1);
     const totalPages = 8;
 
@@ -14,6 +75,10 @@ export default function Notify() {
             currentPage={currentPage}
             totalPages={totalPages}
             handlePageChange={handlePageChange}
+            selectedMenu={selectedMenu}
+            setSelectedMenu={setSelectedMenu}
+            sideMenus={sideMenus}
+            data={data}
         />
     )
 }
--- src/components/units/notify/Notify.presenter.jsx
+++ src/components/units/notify/Notify.presenter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { renderPagination } from "@/src/components/common/layout/table/pagination";
 
-export default function NotifyUI({ currentPage, totalPages, handlePageChange }) {
+export default function NotifyUI({ currentPage, totalPages, handlePageChange, selectedMenu, setSelectedMenu, sideMenus, data }) {
     return (
         <div className="flex flex-col gap-7 p-5 h-full">
             <div className="flex flex-col gap-2">
@@ -12,81 +12,51 @@ export default function NotifyUI({ currentPage, totalPages, handlePageChange }) 
 
             <div className="flex flex-row gap-3 w-full h-full">
                 <div className="flex flex-col gap-2 w-[210px] min-w-[140px] text-[15px]">
-                    <div
-                        className="flex flex-row justify-between cursor-pointer border border-solid border-[#1E7EE4] rounded-lg px-5 py-3 bg-[#F0F7FF]">
-                        <p className="weight-700 text-[#1E7EE4]">동아리</p>
-                        <p className="text-[#808080] weight-500">3</p>
-                    </div>
-                    <div
-                        className="flex flex-row justify-between cursor-pointer border border-solid border-[#DBDBDB] rounded-lg px-5 py-3 bg-white">
-                        <p className="weight-600">예산</p>
-                        <p className="text-[#808080] weight-500">3</p>
-                    </div>
-                    <div
-                        className="flex flex-row justify-between cursor-pointer border border-solid border-[#DBDBDB] rounded-lg px-5 py-3 bg-white">
-                        <p className="weight-600">통장 관리</p>
-                        <p className="text-[#808080] weight-500">3</p>
-                    </div>
+                    {Object.keys(sideMenus).map((menu) => (
+                        <div
+                            key={menu}
+                            className={`flex flex-row justify-between cursor-pointer border border-solid rounded-lg px-5 py-3 hover:bg-[#F0F7FF] hover:border-[#1E7EE4] hover:text-[#1E7EE4] transform duration-300
+                                ${selectedMenu === menu ? 'border-[#1E7EE4] bg-[#F0F7FF]' : 'bg-white'}`}
+                            onClick={() => setSelectedMenu(menu)}>
+                            <p className={`${selectedMenu === menu ? 'weight-700 text-[#1E7EE4]' : 'weight-600'}`}>{menu}</p>
+                            <p className="text-[#808080] weight-500">3</p>
+                        </div>
+                    ))}
                 </div>
 
-                <div className="flex flex-col w-full h-full">
+                <div className="flex flex-col w-full h-fit">
                     <div
-                        className="w-full h-[90%] border border-solid border-[#DBDBDB] rounded-lg px-8 py-6 bg-white">
-                        <div className="flex flex-row justify-between p-1">
-                            <div className="flex flex-col justify-between pr-4">
-                                <div className="flex flex-row items-center gap-2 pb-1.5">
-                                    <p className="weight-700">결제 승인 요청</p>
-                                    <p className="text-[#1E7EE4] text-[14px] weight-500">New</p>
-                                </div>
+                        className="flex-grow overflow-auto w-full h-fit border border-solid border-[#DBDBDB] rounded-lg px-8 py-2 bg-white">
+                        {data.length > 0 ? (
+                            data.map((item, index) => (
+                                <>
+                                    <div className="flex flex-row justify-between px-1 py-4" key={index}>
+                                        <div className="flex flex-col justify-between pr-4">
+                                            <div className="flex flex-row items-center gap-2 pb-1.5">
+                                                <p className="weight-700">{item.title}</p>
+                                                {item.isNew && (
+                                                    <p className="text-[#1E7EE4] text-[14px] weight-500">New</p>
+                                                )}
+                                            </div>
 
-                                <p className="text-[14px] text-[#4C545B] weight-500">[DEVELOPER] 2025.03.10 카누 외 1건 승인
-                                    요청</p>
+                                            <p className="text-[14px] text-[#4C545B] weight-500">{item.description}</p>
+                                        </div>
+
+                                        <div className="flex flex-col justify-center pl-4">
+                                            <p className="text-[#4C545B] text-[12px]">{item.date}</p>
+                                        </div>
+                                    </div>
+
+                                    {index < data.length - 1 && (
+                                        <hr className="border-[0.5px] border-solid border-[#DBDBDB]"/>
+                                    )}
+                                </>
+                            ))
+                        ) : (
+                            <div className="flex flex-col justify-center items-center h-full">
+                                <p className="text-[#4C545B] text-[14px] weight-500">알림이 없습니다.</p>
                             </div>
-
-                            <div className="flex flex-col justify-center pl-4">
-                                <p className="text-[#4C545B] text-[12px]">2025.03.15</p>
-                            </div>
-                        </div>
-
-                        <hr className="my-4 border-[0.5px] border-solid border-[#DBDBDB]"/>
-
-
-                        <div className="flex flex-row justify-between p-1">
-                            <div className="flex flex-col justify-between pr-4">
-                                <div className="flex flex-row items-center gap-2 pb-1.5">
-                                    <p className="weight-700">결제 승인 요청</p>
-                                    <p className="text-[#1E7EE4] text-[14px] weight-500">New</p>
-                                </div>
-
-                                <p className="text-[14px] text-[#4C545B] weight-500">[DEVELOPER] 2025.03.10 카누 외 1건 승인
-                                    요청</p>
-                            </div>
-
-                            <div className="flex flex-col justify-center pl-4">
-                                <p className="text-[#4C545B] text-[12px]">2025.03.15</p>
-                            </div>
-                        </div>
-
-                        <hr className="my-4 border-[0.5px] border-solid border-[#DBDBDB]"/>
-
-
-                        <div className="flex flex-row justify-between p-1">
-                            <div className="flex flex-col justify-between pr-4">
-                                <div className="flex flex-row items-center gap-2 pb-1.5">
-                                    <p className="weight-700">결제 승인 요청</p>
-                                </div>
-
-                                <p className="text-[14px] text-[#4C545B] weight-500">요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청요청</p>
-                            </div>
-
-                            <div className="flex flex-col justify-center pl-4">
-                                <p className="text-[#4C545B] text-[12px]">2025.03.15</p>
-                            </div>
-                        </div>
-
-                        <hr className="my-4 border-[0.5px] border-solid border-[#DBDBDB]"/>
-
-
+                        )}
                     </div>
                     <div
                         className="content-center text-center w-full h-[10%]">
