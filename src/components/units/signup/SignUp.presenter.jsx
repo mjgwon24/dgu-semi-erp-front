25cdeb7343f8c10ec4ac11a1724f454c4df8c1e0
Hwajinpo <hwajindeck@gmail.com>
2025-04-06 22:58:10 +0900

    refactor: 회원가입 페이지 구조변경
    약관 페이지 signup/signupContract로 분리

--- src/components/units/signup/SignUp.presenter.jsx
+++ src/components/units/signup/SignUp.presenter.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { ConfigProvider, Checkbox, Select, Input } from "antd";
 import { useState } from "react";
+import SignUpContract from "./signupContract/SignUpContract.container";
 
 export default function SignUpUI({
     email,
@@ -18,7 +19,6 @@ export default function SignUpUI({
     allChecked,
     clubOptions,
     majorOptions,
-    openContractModal,
     openContract,
     closeContract,
     isContractOpen,
@@ -43,112 +43,21 @@ export default function SignUpUI({
                 },
             }}
             >
-
-
                     
         <div className="flex w-full h-screen justify-center items-center bg-[#F7F7F7]">
             <div className="flex flex-col items-center border rounded-2xl shadow-xl bg-[#FFFFFF] h-5/6 py-14 px-32 w-2/5 min-w-[600px]">
             {isContractOpen ? 
             
             (
-                <div className="w-full">
-                    <button type="button" onClick={closeContract} className="text-black hover:text-[#808080] focus:outline-none focus:ring-0 bg-transparent">
-                        <svg class="w-5 h-5 transform rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                    </button>
-                    <div className="flex flex-col items-center w-full gap-8">
-                        <label className="text-3xl font-[700]">전체 약관</label>
-
-                        <div className="flex flex-col gap-2 p-4 max-h-[470px] overflow-y-auto">
-                        <div className="flex flex-col gap-2">
-                            <label className="text-xl font-[700]">소제목 (필수)</label>
-                            <div className="max-h-40 overflow-y-auto border border-[#DBDBDB] p-6 rounded-lg text-[#3C3C3C]">
-                            <p className="whitespace-pre-line">
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-
-                            </p>
-                            </div>
-                            <Checkbox className=" text-[#1E7EE4]"name="option1" checked={checkedList.option1} onChange={onCheckChange}>동의</Checkbox>
-                        </div>
-
-                        <div className="flex flex-col gap-2">
-                            <label className="text-xl font-[700]">소제목</label>
-                            <div className="max-h-40 overflow-y-auto border border-[#DBDBDB] p-6 rounded-lg text-[#3C3C3C]">
-                            <p className="whitespace-pre-line">
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-
-                            </p>
-                            </div>
-                            <Checkbox className=" text-[#1E7EE4]"name="option2" checked={checkedList.option2} onChange={onCheckChange}>동의</Checkbox>
-                        </div>
-
-                        <div className="flex flex-col gap-2">
-                            <label className="text-xl font-[700]">소제목</label>
-                            <div className="max-h-40 overflow-y-auto border border-[#DBDBDB] p-6 rounded-lg text-[#3C3C3C]">
-                            <p className="whitespace-pre-line">
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-                                약관내용 어쩌고 저쩌고 이렇고 저렇고 뭐시기 거시기
-
-                            </p>
-                            </div>
-                            <Checkbox className=" text-[#1E7EE4]"name="option3" checked={checkedList.option3} onChange={onCheckChange}>동의</Checkbox>
-                        </div>
-                        
-                    </div>
-                        <button className="bg-[#237BE6] text-[#FFFFFF] rounded-lg w-full text-xl p-4" type="button" onClick={onAgreeAllClick}>전체 동의</button>
-                    </div>
-                </div>
+                <SignUpContract
+                    allChecked={allChecked}
+                    checkedList={checkedList}
+                    isContractOpen={isContractOpen}
+                    onCheckChange={onCheckChange}
+                    onCheckAllChange={onCheckAllChange}
+                    onAgreeAllClick={onAgreeAllClick}
+                    closeContract={closeContract}
+                />
             ) : (
                 <form className="flex flex-col gap-8 w-full" onSubmit={handleSubmit}>
                 <div className="flex flex-col items-center gap-1">
@@ -159,10 +68,8 @@ export default function SignUpUI({
 
                 <div className="flex flex-col gap-2 w-full text-lg">
                     <Input placeholder="학번" style={{height:"62px", padding:"11px"}}/> 
-                    {/* <input className="border border-[#DBDBDB] rounded-lg p-4 text-[#000000]" type="text" placeholder="학번" onChange={onStudentNumChange}></input> */}
                     <Select placeholder="전공" options={majorOptions} style={{height:"62px"}}></Select>
                     <Select placeholder="동아리" options={clubOptions} style={{height:"62px"}}></Select>
-                    {/* <input className="border border-[#DBDBDB] rounded-lg p-4 text-[#000000]" type="text" placeholder="이메일" onChange={onEmailChange}></input> */}
                     <div className="flex flex-row w-full gap-2">
                         <Input placeholder="이메일" style={{height:"62px", padding:"11px"}}/> 
                         <button className="bg-[#237BE6] rounded-lg text-base px-10 text-[#FFFFFF]" style={{ height: "62px", whiteSpace: "nowrap" }}>인증</button>
@@ -183,10 +90,6 @@ export default function SignUpUI({
                         <button className="text-[#808080] text-base" onClick={openContract}>&gt;</button>
                     </div>
                     
-                    
-
-                    {/* <Checkbox className="text-lg text-[#3C3C3C]">19세 이상??</Checkbox>
-                    <Checkbox className="text-lg text-[#3C3C3C]">이용약관 동의</Checkbox> */}
                 </div>
 
                 <div className="flex flex-col gap-2 w-full items-center">
