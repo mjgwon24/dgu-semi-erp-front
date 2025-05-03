1a3e6e0f3f0c0cd1ff19a79d1a1ae809328ec295
mjgwon24 <alswlchlrh8@naver.com>
2025-05-03 14:00:50 +0900

    update: 사이드바를 닫을 경우 아이콘 노출

--- src/components/common/layout/sidebar/index.jsx
+++ src/components/common/layout/sidebar/index.jsx
@@ -16,21 +16,18 @@ export default function SideBarLayout() {
     const currentUrl = usePathname();
     const [isSidebarFolded, setIsSidebarFolded] = useState(false);
 
-    const menuBaseClass = "flex items-center cursor-pointer space-x-3 rounded-md py-2 px-3 hover:bg-white hover:bg-opacity-25 w-full";
+    const menuBaseClass = "flex items-center cursor-pointer space-x-3 rounded-md py-2 px-3.5 hover:bg-white hover:bg-opacity-25 w-full h-10";
     const textBaseClass = "select-none weight-700 text-[#FDFDFD]";
 
     return (
         <div className="flex flex-col justify-between h-full bg-gradient-to-b from-[#4A96EC] via-[#4A96EC] to-[#237BE6]"
-                style={{
-                        width: isSidebarFolded ? "70px" : "230px"
-                    }}
-        >
+                style={{width: isSidebarFolded ? "70px" : "230px"}}>
             <div className="flex flex-col"
                  style={{padding: isSidebarFolded ? "30px 10px 20px 10px"
                          : "30px 20px 20px 20px"}}>
 
                 <Link href="/main"
-                      className="cursor-pointer"
+                      className="cursor-pointer min-h-[54px]"
                       onClick={() => setBudgetMenuOpen(false)}>
                     <h1 className="text-white weight-700 text-[17px]"
                         style={{fontSize: isSidebarFolded ? "10px" : "18px",
@@ -40,15 +37,17 @@ export default function SideBarLayout() {
                     </h1>
                 </Link>
 
-                <div className="flex flex-col space-y-3 pt-10" style={{ display: isSidebarFolded ? "none" : "" }}>
+                <div className="flex flex-col space-y-3 pt-10">
                     <Link href="/announcement"
                           className={`${menuBaseClass} ${currentUrl === "/announcement" ? "bg-white text-[#106BDB]" : "text-white"}`}
                           style={{color: currentUrl === "/announcement" ? "#106BDB" : "", backgroundColor: currentUrl === "/announcement" ? "white" : "",}}
                           onClick={() => setBudgetMenuOpen(false)}>
                         <ArticleIcon fontSize="small" />
-                        <span className={textBaseClass} style={{color: currentUrl === "/announcement" ? "#106BDB" : "",}}>
-                            공지사항
-                        </span>
+                        {!isSidebarFolded && (
+                            <span className={textBaseClass} style={{color: currentUrl === "/announcement" ? "#106BDB" : "",}}>
+                                공지사항
+                            </span>
+                        )}
                     </Link>
 
                     <Link href="/main"
@@ -56,10 +55,12 @@ export default function SideBarLayout() {
                           style={{color: currentUrl === "/clubManagement" ? "#106BDB" : "", backgroundColor: currentUrl === "/clubManagement" ? "white" : "",}}
                           onClick={() => setBudgetMenuOpen(false)}>
                         <GroupsIcon fontSize="small" />
-                        <span className={textBaseClass}
-                              style={{color: currentUrl === "/clubManagement" ? "#106BDB" : "",}}>
-                            동아리
-                        </span>
+                        {!isSidebarFolded && (
+                            <span className={textBaseClass}
+                                  style={{color: currentUrl === "/clubManagement" ? "#106BDB" : "",}}>
+                                동아리
+                            </span>
+                        )}
                     </Link>
 
                     <div className={`${menuBaseClass} justify-between ${isBudgetMenuOpen ? "bg-[#EFF7FF]" : ""}`}
@@ -91,29 +92,33 @@ export default function SideBarLayout() {
                                             ? "#106BDB"
                                             : "white"
                             }}/>
-                            <span
-                                className={textBaseClass}
-                                style={{
-                                    color:
-                                        currentUrl === "/budgetPlan" ||
-                                        currentUrl === "/budgetUsage" ||
-                                        currentUrl === "/budgetReport" ||
-                                        isBudgetMenuOpen
-                                            ? "#106BDB"
-                                            : "",
-                                }}>
-                                예산
-                            </span>
+                            {!isSidebarFolded && (
+                                <span
+                                    className={textBaseClass}
+                                    style={{
+                                        color:
+                                            currentUrl === "/budgetPlan" ||
+                                            currentUrl === "/budgetUsage" ||
+                                            currentUrl === "/budgetReport" ||
+                                            isBudgetMenuOpen
+                                                ? "#106BDB"
+                                                : "",
+                                    }}>
+                                    예산
+                                </span>
+                            )}
                         </div>
 
-                        <span className="flex items-center">
-                            {isBudgetMenuOpen ? (
-                                <ExpandLessIcon fontSize="small" style={{ color: "#106BDB" }} />) : (
-                                <ExpandMoreIcon fontSize="small" style={{ color: "#FDFDFD" }} />)}
-                        </span>
+                        {!isSidebarFolded && (
+                            <span className="flex items-center">
+                                {isBudgetMenuOpen ? (
+                                    <ExpandLessIcon fontSize="small" style={{ color: "#106BDB" }} />) : (
+                                    <ExpandMoreIcon fontSize="small" style={{ color: "#FDFDFD" }} />)}
+                            </span>
+                        )}
                     </div>
 
-                    {isBudgetMenuOpen && (
+                    {isBudgetMenuOpen && !isSidebarFolded && (
                         <div className="ml-2 mt-2 flex flex-col space-y-2">
                             <Link href="/budgetPlan"
                                   className="text-gray-100 cursor-pointer text-[15px] weight-700 rounded-md py-1.5 px-3 hover:bg-white hover:bg-opacity-25 w-full flex items-center"
@@ -143,10 +148,12 @@ export default function SideBarLayout() {
                           style={{color: currentUrl === "/bankbookManagement" ? "#106BDB" : "", backgroundColor: currentUrl === "/bankbookManagement" ? "white" : "",}}
                           onClick={() => setBudgetMenuOpen(false)}>
                         <PointOfSaleIcon fontSize="small" />
-                        <span className={textBaseClass}
-                              style={{color: currentUrl === "/bankbookManagement" ? "#106BDB" : "",}}>
-                            통장관리
-                        </span>
+                        {!isSidebarFolded && (
+                            <span className={textBaseClass}
+                                  style={{color: currentUrl === "/bankbookManagement" ? "#106BDB" : "",}}>
+                                통장관리
+                            </span>
+                        )}
                     </Link>
 
                     <Link href="/peopleManagement"
@@ -154,10 +161,12 @@ export default function SideBarLayout() {
                           style={{color: currentUrl === "/peopleManagement" ? "#106BDB" : "", backgroundColor: currentUrl === "/peopleManagement" ? "white" : "",}}
                           onClick={() => setBudgetMenuOpen(false)}>
                         <PersonSearchIcon fontSize="small" />
-                        <span className={textBaseClass}
-                              style={{color: currentUrl === "/peopleManagement" ? "#106BDB" : "",}}>
-                            인원관리
-                        </span>
+                        {!isSidebarFolded && (
+                            <span className={textBaseClass}
+                                  style={{color: currentUrl === "/peopleManagement" ? "#106BDB" : "",}}>
+                                인원관리
+                            </span>
+                        )}
                     </Link>
 
                     <Link href="/generalSchedule"
@@ -165,10 +174,12 @@ export default function SideBarLayout() {
                           style={{color: currentUrl === "/generalSchedule" ? "#106BDB" : "", backgroundColor: currentUrl === "/generalSchedule" ? "white" : "",}}
                           onClick={() => setBudgetMenuOpen(false)}>
                         <CalendarMonthIcon fontSize="small" />
-                        <span className={textBaseClass}
-                              style={{color: currentUrl === "/generalSchedule" ? "#106BDB" : "",}}>
-                            일정
-                        </span>
+                        {!isSidebarFolded && (
+                            <span className={textBaseClass}
+                                  style={{color: currentUrl === "/generalSchedule" ? "#106BDB" : "",}}>
+                                일정
+                            </span>
+                        )}
                     </Link>
                 </div>
             </div>
